7f31d3fba6b5a680b30872e34248a655514dd7f3
Moh1020hus <120282683+Moh1020hus@users.noreply.github.com>
2026-02-23 13:32:18 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,11 +2,10 @@ import gymnasium as gym
 from stable_baselines3 import SAC
 from CrazyflieFollowEnv import CrazyflieFollowEnv
 
-# 1. Load the Environment (With Visuals ON this time!)
+# 1. Load the Environment 
 env = CrazyflieFollowEnv(render_mode="human")
 
 # 2. Load the Trained Brain
-# Make sure the filename matches what you saved!
 model = SAC.load("final_drone_model.zip")
 
 # 3. Enjoy the Show
@@ -14,12 +13,12 @@ obs, _ = env.reset()
 print("Test started! Press Ctrl+C to stop.")
 
 while True:
-    # Ask the AI what to do
+   
     action, _states = model.predict(obs, deterministic=True)
     
-    # Do it
+
     obs, reward, done, truncated, info = env.step(action)
     
-    # If the drone crashes or time runs out, reset
+    
     if done or truncated:
         obs, _ = env.reset()
